6f85e9b3e2efbfdfab0e0d23c089af401c6bb3f8
Om Gupta <guptaomg23@gmail.com>
2026-04-24 01:40:41 +0530

    Learn About Multiple Route Handler

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from hello");
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Route Handler 1");
+    next();
+    // res.send("Fetched 1");
+  },
+  (req, res, next) => {
+    console.log("Route Handler 2");
+    next();
+    // res.send("Fetched 2");
+  },
 
-app.use("/", (req, res) => {
-  res.send("Hello from the server");
-});
+  (req, res, next) => {
+    console.log("Route Handler 3");
+    res.send("Fetched 3");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
