74ae66ceea4fc90ddba05fd4bc6cc97daac0f8a0
Om Gupta <guptaomg23@gmail.com>
2026-05-06 16:15:25 +0530

    update backend

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const userrooter = require("./Router/user");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://dev-connect-ui-6gyv.vercel.app"],
     credentials: true,
   }),
 );
